a33daa18884dd83259538c543b8f107c38c6ebd8
Diego Martins <diego@hypersoft.com.br>
2020-02-20 20:19:21 -0300

    Adicionado mensagem de status

--- boletim.php
+++ boletim.php
@@ -7,3 +7,9 @@ $media = ($nota1 + $nota2) / 2;
 echo 'Nota 1: ' . $nota1 . PHP_EOL;
 echo 'Nota 2: ' . $nota2 . PHP_EOL;
 echo 'Média: ' . $media . PHP_EOL;
+
+if ($media >= 6) {
+    echo 'Você foi aprovado' . PHP_EOL;
+} else {
+    echo 'Você foi reprovado.' . PHP_EOL;
+}
