98e0c4966e228707f78ef99223178ba6cd75bf9b
Diego Martins <diego@hypersoft.com.br>
2020-02-20 20:23:21 -0300

    Status em cores

--- boletim.php
+++ boletim.php
@@ -1,15 +1,18 @@
 <?php
 
 $nota1 = rand(0, 10);
+$status1 = $nota1 >= 6 ? 'blue' : 'red';
 $nota2 = rand(0, 10);
+$status2 = $nota2 >= 6 ? 'blue' : 'red';
 $media = ($nota1 + $nota2) / 2;
+$statusMedia = $media >= 6 ? 'blue' : 'red';
 
-echo 'Nota 1: ' . $nota1 . PHP_EOL;
-echo 'Nota 2: ' . $nota2 . PHP_EOL;
-echo 'Média: ' . $media . PHP_EOL;
+echo 'Nota 1 (' . $status1 . '): ' . $nota1 . PHP_EOL;
+echo 'Nota 2 (' . $status2 . '): ' . $nota2 . PHP_EOL;
+echo 'Média (' . $statusMedia . '): ' . $media . PHP_EOL;
 
 if ($media >= 6) {
-    echo 'Você foi aprovado' . PHP_EOL;
+    echo '(' . $statusMedia . ')Você foi aprovado' . PHP_EOL;
 } else {
-    echo 'Você foi reprovado.' . PHP_EOL;
+    echo '(' . $statusMedia . ')Você foi reprovado.' . PHP_EOL;
 }
