50c3df6abb84b10f3f1d53ad7840152b7889f556
Diego Martins <diego@hypersoft.com.br>
2020-02-26 19:49:30 -0300

    Notas 3 e 4 adicionadas

--- boletim.php
+++ boletim.php
@@ -4,11 +4,18 @@ $nota1 = rand(0, 10);
 $status1 = $nota1 >= 6 ? 'blue' : 'red';
 $nota2 = rand(0, 10);
 $status2 = $nota2 >= 6 ? 'blue' : 'red';
-$media = ($nota1 + $nota2) / 2;
+$nota3 = rand(0, 10);
+$status3 = $nota3 >= 6 ? 'blue' : 'red';
+$nota4 = rand(0, 10);
+$status4 = $nota4 >= 6 ? 'blue' : 'red';
+
+$media = ($nota1 + $nota2 + $nota3 + $nota4) / 4;
 $statusMedia = $media >= 6 ? 'blue' : 'red';
 
 echo 'Nota 1 (' . $status1 . '): ' . $nota1 . PHP_EOL;
 echo 'Nota 2 (' . $status2 . '): ' . $nota2 . PHP_EOL;
+echo 'Nota 3 (' . $status3 . '): ' . $nota3 . PHP_EOL;
+echo 'Nota 4 (' . $status4 . '): ' . $nota4 . PHP_EOL;
 echo 'Média (' . $statusMedia . '): ' . $media . PHP_EOL;
 
 if ($media >= 6) {
@@ -16,5 +23,3 @@ if ($media >= 6) {
 } else {
     echo '(' . $statusMedia . ')Você foi reprovado.' . PHP_EOL;
 }
-
-echo 'Versão 2 com recurso extra' . PHP_EOL;
